957c70874cf1927026e94f8d18a491496574a778
revoulce <54038231+revoulce@users.noreply.github.com>
2021-12-07 20:17:10 +0300

    Новое активити

--- app/src/main/java/pro/hakta/mad/mskko2021/Login.kt
+++ app/src/main/java/pro/hakta/mad/mskko2021/Login.kt
@@ -1,11 +1,27 @@
 package pro.hakta.mad.mskko2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    fun signIn(view: View) {
+
+    }
+
+    fun register(view: View) {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+    }
+
+    fun goToProfile(view: View) {
+
+    }
+
 }
